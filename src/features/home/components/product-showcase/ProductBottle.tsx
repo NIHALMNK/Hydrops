36398aa6a66697e1355f0967d45d02c41d0480c1
MOHAMMED NIHAL K <keedathnihal@gmail.com>
2026-07-18 22:00:29 +0530

    feat: add ProductBottle component with responsive styling and parallax effects

--- src/features/home/components/product-showcase/ProductBottle.tsx
+++ src/features/home/components/product-showcase/ProductBottle.tsx
@@ -4,7 +4,7 @@ export function ProductBottle() {
   return (
     <div className="product-bottle-container flex-1 w-full flex items-center justify-center relative perspective-[1000px] h-[50vh] md:h-auto">
       <div className="product-bottle will-change-transform opacity-0 scale-90 origin-center">
-        <div className="product-parallax relative w-[240px] md:w-[320px] lg:w-[400px] aspect-[1/2.2]">
+        <div className="product-parallax relative h-[42vh] md:h-[52vh] lg:h-[62vh] aspect-[1/2.2]">
           
           {/* Soft Shadow below bottle */}
           <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 w-[60%] h-[20px] bg-black/10 blur-xl rounded-[100%] scale-x-[1.5]"></div>
@@ -14,7 +14,7 @@ export function ProductBottle() {
             alt="Hydrops Coconut Oil Bottle"
             fill
             className="object-contain drop-shadow-2xl z-10"
-            sizes="(max-width: 768px) 240px, (max-width: 1024px) 320px, 400px"
+            sizes="(max-height: 768px) 45vh, (max-height: 1024px) 55vh, 65vh"
             priority
           />
           
